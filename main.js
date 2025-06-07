423779d72f454c33d8e2e3c7011147db85c94903
xemzreal <xemztzy@gmail.com>
2025-06-07 13:52:14 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+var typed = new Typed(".text", {
+
+    strings: ["Computer Science Student", "Frontend Developer", "Web Developer"],
+
+    typeSpeed: 100,
+
+    backSpeed: 100,
+
+    backDelay: 1000,
+
+    loop: true
+
+});
